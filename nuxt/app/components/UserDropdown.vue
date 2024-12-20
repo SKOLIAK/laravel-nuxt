16d098c2aa9329bf64c37b0563d0dc6a9ef4b9bb
SKOLIAK <132194891+SKOLIAK@users.noreply.github.com>
2024-12-20 13:43:48 +0000

    Client: Final UserDropdown

--- nuxt/app/components/UserDropdown.vue
+++ nuxt/app/components/UserDropdown.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-const { isHelpSlideoverOpen } = useDashboard()
-const { isDashboardSearchModalOpen } = useUIState()
-const { metaSymbol } = useShortcuts()
+const auth = useAuthStore();
+const { $storage } = useNuxtApp();
+import dayjs  from 'dayjs';
 
 const items = computed(() => [
   [{
@@ -11,46 +11,38 @@ const items = computed(() => [
   }], [{
     label: 'Settings',
     icon: 'i-heroicons-cog-8-tooth',
-    to: '/settings'
-  }, {
-    label: 'Command menu',
-    icon: 'i-heroicons-command-line',
-    shortcuts: [metaSymbol.value, 'K'],
-    click: () => {
-      isDashboardSearchModalOpen.value = true
-    }
-  }, {
-    label: 'Help & Support',
-    icon: 'i-heroicons-question-mark-circle',
-    shortcuts: ['?'],
-    click: () => isHelpSlideoverOpen.value = true
-  }], [{
-    label: 'Documentation',
-    icon: 'i-heroicons-book-open',
-    to: 'https://ui.nuxt.com/pro/getting-started',
-    target: '_blank'
-  }, {
-    label: 'GitHub repository',
-    icon: 'i-simple-icons-github',
-    to: 'https://github.com/nuxt-ui-pro/dashboard',
-    target: '_blank'
-  }, {
-    label: 'Buy Nuxt UI Pro',
-    icon: 'i-heroicons-credit-card',
-    to: 'https://ui.nuxt.com/pro/purchase',
-    target: '_blank'
+    to: '/account/general'
+  },{
+    label: 'Devices',
+    icon: 'i-heroicons-device-phone-mobile',
+    to: '/account/devices'
   }], [{
     label: 'Sign out',
-    icon: 'i-heroicons-arrow-left-on-rectangle'
+    icon: 'i-heroicons-arrow-left-on-rectangle',
+    click: auth.logout
   }]
 ])
+
+const getGreeting = () => {
+    const currentTime = +dayjs().format('HH')
+    if (currentTime >= 6 && currentTime < 12) {
+      return "Good morning"
+    } else if (currentTime >= 12 && currentTime < 18) {
+      return "Good afternoon"
+    } else if (currentTime >= 18 && currentTime < 22) {
+      return "Good evening"
+    } else {
+      return "Good night"
+    }
+  }
 </script>
 
 <template>
   <UDropdown
+    v-if="auth.logged"
     mode="hover"
     :items="items"
-    :ui="{ width: 'w-full', item: { disabled: 'cursor-text select-text' } }"
+    :ui="{ width: 'min-w-52 w-full', item: { disabled: 'cursor-text select-text' } }"
     :popper="{ strategy: 'absolute', placement: 'top' }"
     class="w-full"
   >
@@ -59,34 +51,43 @@ const items = computed(() => [
         color="gray"
         variant="ghost"
         class="w-full"
-        label="Benjamin"
         :class="[open && 'bg-gray-50 dark:bg-gray-800']"
       >
         <template #leading>
           <UAvatar
-            src="https://avatars.githubusercontent.com/u/739984?v=4"
-            size="2xs"
+            size="sm"
+            :src="$storage(auth.user.avatar)"
+            :alt="auth.user.name"
+            :ui="{ rounded: 'rounded-md' }"
           />
         </template>
 
         <template #trailing>
-          <UIcon
+          <!-- <UIcon
             name="i-heroicons-ellipsis-vertical"
             class="w-5 h-5 ml-auto"
-          />
+          /> -->
         </template>
       </UButton>
     </template>
 
     <template #account>
       <div class="text-left">
-        <p>
-          Signed in as
+        <p class="text-xs">
+          {{ getGreeting() }},
         </p>
         <p class="truncate font-medium text-gray-900 dark:text-white">
-          ben@nuxtlabs.com
+          {{ auth.user.name }}
         </p>
       </div>
     </template>
   </UDropdown>
+  <UButton
+    v-else
+    color="gray"
+    variant="ghost"
+    class="w-full"
+    label="Sign In"
+    to="/auth/login"
+  />
 </template>
