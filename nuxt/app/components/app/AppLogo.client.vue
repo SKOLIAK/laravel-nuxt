0fe4ea6450be1bbb36755fa2b73e435038b5b586
SKOLIAK <132194891+SKOLIAK@users.noreply.github.com>
2024-12-20 13:41:26 +0000

    Add Logo

---
+++ nuxt/app/components/app/AppLogo.client.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+<template>
+  <NuxtLink to="/" class="font-medium text-lg flex items-center text-black dark:text-white leading-tight tracking-tight">
+    <img src="@/assets/images/logo-dark.svg" class="mr-1 dark:hidden"/>
+    <img src="@/assets/images/logo.svg" class="mr-1 hidden dark:block"/>
+    tradenote
+  </NuxtLink>
+</template>
