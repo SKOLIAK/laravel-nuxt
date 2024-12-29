3e32120dd00cf278959b9b021154ef7b1d6ab7e0
SKOLIAK <132194891+SKOLIAK@users.noreply.github.com>
2024-12-29 18:34:16 +0000

    Expand/Collapse

--- nuxt/app/components/DEV/block.vue
+++ nuxt/app/components/DEV/block.vue
@@ -1,6 +1,23 @@
 <template>
       <div class="bg-black p-8 text-white rounded-lg mb-4 text-sm">
-        <div class="tracking-tighter font-medium"><slot name="title"/></div>
-        <code class="bg-white/10 p-0.5 text-xs !font-bold text-primary rounded"><slot name="code"/></code>
+        <div class="flex items-center justify-start">
+          <div class="tracking-tighter font-medium grow "><slot name="title"/></div>
+          <button class="ml-auto text-xs font-medium text-white/80" @click="collapseExpand">
+            <span v-if="isCollapsed">Expand</span>
+            <span v-else>Collapse</span>
+          </button>
+        </div>
+        <div class="h-full transition duration-500 overflow-hidden" :class="{'!h-0': isCollapsed}">
+          <code class="bg-white/10 p-0.5 text-xs !font-bold text-primary rounded"><slot name="code"/></code>
+        </div>
       </div>
 </template>
+
+<script lang="ts" setup>
+const isCollapsed = ref(false)
+
+function collapseExpand() {
+  isCollapsed.value = !isCollapsed.value
+}
+
+</script>
