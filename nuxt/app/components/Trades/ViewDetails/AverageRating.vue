c3810aa4c68cdb99c4be5b437c7126213c29d8f1
SKOLIAK <132194891+SKOLIAK@users.noreply.github.com>
2025-01-02 01:00:20 +0000

    Styling update

--- nuxt/app/components/Trades/ViewDetails/AverageRating.vue
+++ nuxt/app/components/Trades/ViewDetails/AverageRating.vue
@@ -2,10 +2,11 @@
   <p
     class="text-xs select-none font-bold inline-flex items-center py-0.5 px-1.5 rounded  text-black/90 mr-1 bg-gray-200"
     :class="{
-      'bg-green-600 text-white drop-shadow': rating >= 7,
-      'bg-gray-500 text-white drop-shadow': rating <= 7 && rating >= 5,
-      'bg-gray-700 text-white drop-shadow': rating <= 5 && rating >= 3,
-      'bg-gray-900 text-white drop-shadow': rating <= 3 && rating > 0
+      'rating-8-up': rating >= 8,
+      'rating-7-up': rating >= 7 && rating <= 8,
+      'rating-5-up': rating <= 7 && rating >= 5,
+      'rating-3-up': rating >= 3 && rating <= 5,
+      'rating-0-up': rating <= 3 && rating > 0
     }"
   >
     <span v-if="rating > 0">{{ rating }}</span>
